b02448973c786982d9c909f9b3060e8abb0a98d5
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2022-10-11 16:45:04 -0400

    create unit tests for compiler

---
+++ 1kyu/tiny-three-pass-compiler/fixtures/pass1result.js
@@ -0,0 +1,23 @@
+const pass1Result = {
+  op: "/",
+  a: {
+    op: "-",
+    a: {
+      op: "+",
+      a: {
+        op: "*",
+        a: { op: "*", a: { op: "imm", n: 2 }, b: { op: "imm", n: 3 } },
+        b: { op: "arg", n: 0 },
+      },
+      b: { op: "*", a: { op: "imm", n: 5 }, b: { op: "arg", n: 1 } },
+    },
+    b: { op: "*", a: { op: "imm", n: 3 }, b: { op: "arg", n: 2 } },
+  },
+  b: {
+    op: "+",
+    a: { op: "+", a: { op: "imm", n: 1 }, b: { op: "imm", n: 3 } },
+    b: { op: "*", a: { op: "imm", n: 2 }, b: { op: "imm", n: 2 } },
+  },
+};
+
+export default pass1Result;
--- 1kyu/tiny-three-pass-compiler/index.test.js
+++ 1kyu/tiny-three-pass-compiler/index.test.js
@@ -1,7 +1,27 @@
-import Compiler from './compiler';
+import Compiler from "./compiler";
+import pass1Result from "./fixtures/pass1result";
+import pass2Result from "./fixtures/pass2result";
+import { simulate } from "./simulate";
 
-describe('Compiler' , () => {
-    it('should create an instance', () => {
-        const compiler = new Compiler();
-    });
+const prog = "[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)";
+
+describe("Compiler", () => {
+  it("should create an instance", () => {
+    const compiler = new Compiler();
+  });
+  it("should do pass 1", () => {
+    const compiler = new Compiler();
+    expect(compiler.pass1(prog)).toDeepEqual(pass1Result);
+  });
+  it("should do pass 2", () => {
+    const compiler = new Compiler();
+    expect(compiler.pass2(pass1Result)).toDeepEqual(pass2Result);
+  });
+  it("should do pass 3", () => {
+    const compiler = new Compiler();
+    const p3 = compiler.pass3(pass2Result);
+    expect(simulate(p3, [4, 0, 0])).toEqual(3);
+    expect(simulate(p3, [4, 8, 0])).toEqual(8);
+    expect(simulate(p3, [4, 8, 16])).toEqual(2);
+  });
 });
